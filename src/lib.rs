eae2aa56127aefc5f4bd2b86bfe05c4416856298
Wesley Florence <jw37g@mac.com>
2019-06-21 14:16:04 -0700

    generating mailblocks with the hash

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,43 @@ pub mod crypto;
 
 use wasm_bindgen::prelude::*;
 
+#[macro_use]
+extern crate serde_derive;
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+// Mailblock struct for JSON output
+#[derive(Serialize)]
+struct Header {
+    title: String,
+    version: String
+}
+#[derive(Serialize)]
+struct Mailblock {
+    header: Header,
+    index: i32,
+    total: i32,
+    hash: String
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    let hash = hash_message();
-    let preamble = "Hashed msg: ";
-    let message = format!("{}{}", preamble, hash);
-    alert(&message);
-}
-
-#[wasm_bindgen]
-pub fn hash_message() -> String{
-    let email = String::from("Email stand in string");
-    let hash = crypto::encrypt(&email);
-    hash
+pub fn forge_mailblock(message: &str, index: i32, total: i32) -> JsValue {
+    let hash = crypto::encrypt(message);
+    let mailblock = Mailblock {
+        header: Header {
+            title: String::from("mailblock"),
+            version: String::from("1.0")
+        },
+        index: index,
+        total: total,
+        hash: hash
+    };
+    match JsValue::from_serde(&mailblock) {
+        Ok(json) => json,
+        Err(_) => JsValue::NULL
+    }
 }
